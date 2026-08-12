c5e15ecac206ed0c64f119f9e1c1676c7c16ee62
Simon Cinca <scinca>
2026-08-12 12:21:07 +0200

    pagination?

--- Source/Data/QueryExtensions.cs
+++ Source/Data/QueryExtensions.cs
@@ -3,10 +3,9 @@ namespace Bookmarks;
 public static class QueryExtensions
 {
 
-    public static IQueryable<TEntity> Pagination<TEntity>(this IQueryable<TEntity> query, int page, int pageSize)
-        => query.
-           Skip((page - 1) * pageSize)
+    public static IQueryable<TEntity> Pagination<TEntity>(this IQueryable<TEntity> query,
+                                                          int page, int pageSize)
+        => query
+           .Skip((page - 1) * pageSize)
            .Take(pageSize);
-    
-    
 }
