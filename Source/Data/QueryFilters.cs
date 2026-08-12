1bd883a435cac11f58f023c79af090943fcf6237
Simon Cinca <scinca>
2026-08-12 12:06:21 +0200

    Add static class for QueryFilters so I avoid magic strings

---
+++ Source/Data/QueryFilters.cs
@@ -0,0 +1,8 @@
+namespace Bookmarks;
+
+public static class QueryFilters
+{
+    public const string UserFilter = "A";
+    public const string SoftDeletionFilter = "B";
+    public const string ArchivedFilter = "C";
+}
