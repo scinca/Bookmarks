e1dfbd50b916fde6dff789df14d727ea319fc8a2
Simon Cinca <scinca>
2026-08-13 20:52:38 +0200

    ToEntity

--- Source/Features/Bookmarks/Endpoints/Create/CreateMapper.cs
+++ Source/Features/Bookmarks/Endpoints/Create/CreateMapper.cs
@@ -2,6 +2,16 @@ namespace Bookmarks.Features.Bookmarks.Endpoints.Create;
 
 public class CreateMapper(LinkGenerator linkGenerator) : Mapper<Request, Response, Bookmark>
 {
+    public override Bookmark ToEntity(Request r)
+        => new ()
+        {
+            Name = r.Name ?? r.Url,
+            Url = r.Url,
+            Description = r.Description,
+            GroupId = r.GroupId,
+            OwnerId = r.UserId
+        };
+
     public override Response FromEntity(Bookmark e)
         => new()
         {
