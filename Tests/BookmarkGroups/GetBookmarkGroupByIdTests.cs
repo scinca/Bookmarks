e9554bd9220d5789a7533aee1e1e8713192da636
Simon Cinca <scinca>
2026-08-27 17:11:01 +0200

    valid group id should succeed test

--- Tests/BookmarkGroups/GetBookmarkGroupByIdTests.cs
+++ Tests/BookmarkGroups/GetBookmarkGroupByIdTests.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using Bookmarks;
+using Bookmarks.Features.BookmarkGroups;
 using Bookmarks.Features.BookmarkGroups.Endpoints;
 using Bookmarks.Features.Bookmarks.Endpoints;
 
@@ -16,5 +18,40 @@ public class GetBookmarkGroupByIdTests(App App) : TestBase<App>
         
         rsp.StatusCode.ShouldBe(HttpStatusCode.NotFound);
     }
- 
+
+    [Fact, Priority(2)]
+    public async Task ValidGroupId_ShouldSucceed()
+    {
+        const int id = 2000;
+
+
+        var testGroup = new BookmarkGroup
+        {
+            Id = id,
+            Name = Guid.CreateVersion7().ToString(),
+            Description = null,
+            CreatedAt = DateTime.UnixEpoch,
+            OwnerId = App.UserAId!,
+        };
+        
+        using (var scope = App.Services.CreateScope())
+        {
+            var db = App.Services.GetRequiredService<AppDbContext>();
+            db.BookmarkGroups.Add(testGroup);
+            await db.SaveChangesAsync(TestContext.Current.CancellationToken);
+        }
+
+        var (rsp, res) = await App.UserAClient.GETAsync<GetBookmarkGroupByIdEndpoint, GetBookmarkGroupByIdRequest, GetBookmarkGroupByIdResponse>(new()
+        {
+            Id = id,
+        });
+
+        rsp.StatusCode.ShouldBe(HttpStatusCode.OK);   
+        res.Id.ShouldBe(id);
+        res.Name.ShouldBe(testGroup.Name);
+        res.Description.ShouldBe(testGroup.Description);
+        res.CreatedAt.ShouldBe(testGroup.CreatedAt);
+    }
+    
+    
 }
