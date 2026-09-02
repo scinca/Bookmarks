d5b21942af402ab0763c414d4d8ca81366972c08
Simon Cinca <scinca>
2026-09-02 17:33:47 +0200

    prepare for styling

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,6 +4,8 @@ import {BrowserRouter} from "react-router"
 import './index.css'
 import App from './App.tsx'
 
+import '@fontsource-variable/source-sans-3/wght.css';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
